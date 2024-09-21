7a6e99880aeb2113179296c099554e5a2783d13a
RyotaLab <orengi12260@gmail.com>
2024-09-21 15:41:38 +0900

    Initial Commit

---
+++ ApiSumple/ApiSumpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiSumpleApp.swift
+//  ApiSumple
+//
+//  Created by 渡邊涼太 on 2024/09/21.
+//
+
+import SwiftUI
+
+@main
+struct ApiSumpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ApiSumple/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ApiSumple
+//
+//  Created by 渡邊涼太 on 2024/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
